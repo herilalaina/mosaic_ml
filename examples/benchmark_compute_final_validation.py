f44351a0cdeee01df8133463dace9f28752e1e9e
herilalaina <rkt.herilalaina@gmail.com>
2018-05-18 09:08:48 +0200

    validation file

---
+++ examples/benchmark_compute_final_validation.py
@@ -0,0 +1,85 @@
+import argparse
+import json
+import logging
+import os
+import sys
+sys.path.insert(0,'/home/tau/hrakotoa/Code/mosaic_project/mosaic_ml')
+sys.path.append('.')
+
+
+from update_metadata_util import load_task
+
+import warnings
+import pickle
+import pynisher
+import scipy
+
+from sklearn import datasets, feature_selection, linear_model, feature_selection
+from sklearn.model_selection import train_test_split
+
+from mosaic_ml.automl import AutoML
+from mosaic_ml.utils import balanced_accuracy
+
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import StandardScaler
+
+import glob, os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--working-directory', type=str, required=True)
+parser.add_argument('--time-limit', type=int, required=True)
+parser.add_argument('--per-run-time-limit', type=int, required=True)
+parser.add_argument('--task-id', type=int, required=True)
+parser.add_argument('--task-type', choices=['classification', 'regression'],
+                    required=True)
+parser.add_argument('-s', '--seed', type=int, required=True)
+parser.add_argument('--unittest', action='store_true')
+args = parser.parse_args()
+
+working_directory = args.working_directory
+time_limit = args.time_limit
+per_run_time_limit = args.per_run_time_limit
+task_id = args.task_id
+task_type = args.task_type
+seed = args.seed
+is_test = args.unittest
+
+
+tmp_dir = os.path.join(working_directory, str(task_id))
+
+info = {
+    "working_directory": tmp_dir + '/',
+    "images_directory": tmp_dir + "/images"
+}
+
+
+X_train, y_train, X_test, y_test, cat = load_task(task_id)
+imp = Imputer(missing_values="NaN", strategy="median")
+imp.fit(X_train)
+
+X_train = imp.transform(X_train)
+X_test = imp.transform(X_test)
+
+if not scipy.sparse.issparse(X_train):
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+else:
+    scaler = StandardScaler(with_mean=False)
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+os.chdir(info["working_directory"])
+list_score = []
+for file in sorted(glob.glob('*.pkl'), key=os.path.getmtime):
+    pipeline = pickle.load(open(file, "rb"))
+    pipeline.fit(X_train, y_train)
+    score = balanced_accuracy(y_test, pipeline.predict(X_test))
+    list_score.append((file, score))
+
+with open("{0}/validation.txt".format(tmp_dir), "w") as f:
+    for file, score in list_score:
+        f.write("{0},{1}\n".format(file, score))
+print("Validation created for task={0} run={1}".format(task_id, seed))
